0b3af00e7f64a58ae5a168900dcd8a8a4bb50106
jobneewcont <lisodev382@invodua.com>
2023-01-23 17:46:04 +0500

    I add new information

--- js/script.js
+++ js/script.js
@@ -33,31 +33,41 @@ let arr = [
 
 ]
 let new1 = []
+function user(hhh){
+// let new1 = []
 for( let i = 0; i < arr.length; i++){
     if(arr[i].name.length > 5){
         new1.push(arr[i].name)
     }
 }
+}
+user()
 console.log(new1, 'Ismlari uzuligi 5 dan katta');
 
 let new2 = []
+function addNew(subject){
 for( let i = 0; i < arr.length; i++){
     if(arr[i].name.length < 5){
         new2.push(arr[i].name)
     }
 }
+}
+addNew()
 console.log(new2, 'Ismlari uzuligi 5 dan kichik');
 
 
 let new3 = []
+function addOld(){
 for( let i = 0; i < arr.length; i++){
     if(arr[i].name.length == 5){
         new3.push(arr[i].name)
     }
 }
+}
+addOld()
 console.log(new3, 'Ismlari uzuligi 5 ga teng');
 
-// -------- 2- usul ------------
+// // -------- 2- usul ------------
 
 
 for(let i = 0; i < arr.length; i++){
@@ -71,3 +81,20 @@ for(let i = 0; i < arr.length; i++){
         console.log(arr[i].name, '5 dan kichik');
     }
 }
+
+
+function sayHello(hhh){
+for(let i = 0; i < arr.length; i++){
+    if(arr[i].name.length > 5){
+        console.log(arr[i].name, "ismini uzunligi 5 dan katta");
+    }
+    else if(arr[i].name.length = 5 ){
+        console.log(arr[i].name, "ismini uzunligi 5 ga teng"); 
+    }
+    else {
+        console.log(arr[i].name, '5 dan kichik');
+    }
+}
+}
+
+sayHello()
